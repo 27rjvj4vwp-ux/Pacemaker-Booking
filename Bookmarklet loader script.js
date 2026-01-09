36a468f2b1b5b2642f5c9dd9b10b742ea5928db5
27rjvj4vwp-ux <27rjvj4vwp@privaterelay.appleid.com>
2026-01-09 14:21:14 +0000

    Add bookmarklet loader script

---
+++ Bookmarklet loader script.js
@@ -0,0 +1,1 @@
+javascript:(function()%7B   var s=document.createElement('script');   s.src='https://27rjvj4vwp-ux.github.io/Pacemaker-Booking/Main-Booking.js';   document.head.appendChild(s); %7D)();

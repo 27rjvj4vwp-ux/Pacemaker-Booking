7eaa10be74156adae576b50b56741b43f9b130b7
27rjvj4vwp-ux <27rjvj4vwp@privaterelay.appleid.com>
2026-01-09 12:42:13 +0000

    Add Main-BookingV1-7-2.js file

--- Main-BookingV1-7-2.js
+++ Main-BookingV1-7-2.js
@@ -1,10 +1,10 @@
-// Booking Tool V2.0
+// Booking Tool V1.7.2
 (function () {
 
   // Dynamic publish time for testing
   const defaultPubTime = "07:45";
   const newpubtime = (prompt("Enter publish time (HH:MM) or leave blank for default 07:45:") || defaultPubTime).trim();
-  const teeTime = prompt("Booking tool V2.0 : Pacemakers use only.\nEnter your target tee time (e.g., 09:10):");
+  const teeTime = prompt("Booking tool V1-7-2 : Pacemakers use only.\nEnter your target tee time (e.g., 09:10):");
   if (!teeTime) { alert("No tee time entered."); return; }
 
   // Capture target date
